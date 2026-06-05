24684fd7d4462bd714e81e6d74626c6fd28ab92d
mutie007 <your@email.com>
2026-06-05 21:10:41 +0300

    Add 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,20 +1,12 @@
-i# 1. Create the file
-nano 5-printf.c#include <stdio.h>
+#include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
-# 2. Save and exit nano
-CTRL + X, then Y, then ENTER
-
-# 3. Compile
-gcc -Wall -Werror -Wextra -pedantic -std=gnu89 5-printf.c
-
-# 4. Run
-./a.out
-
-# 5. Check return value
-echo $?
-
