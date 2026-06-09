f6cd6003533f600c8c72014c655186b47792a94a
mutie007 <your@email.com>
2026-06-09 20:43:39 +0300

    Add 4-print_alphabt.c - prints alphabet excluding q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point, prints the alphabet except q and e
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		if (c != 'q' && c != 'e')
+			putchar(c);
+		c++;
+	}
+	putchar('\n');
+
+	return (0);
+}
