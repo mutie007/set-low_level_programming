086f67d473f1767d42cf871028435dc974ce024c
mutie007 <your@email.com>
2026-06-09 21:58:10 +0300

    Add 9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table starting with 0
+ */
+void times_table(void)
+{
+	int i, j, product;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			product = i * j;
+
+			if (j == 0)
+			{
+				_putchar('0');
+			}
+			else
+			{
+				_putchar(',');
+				if (product >= 10)
+					_putchar(' ');
+				else
+					_putchar(' '), _putchar(' ');
+
+				if (product == 0)
+					_putchar('0');
+				else
+				{
+					if (product >= 10)
+						_putchar('0' + product / 10);
+					_putchar('0' + product % 10);
+				}
+			}
+		}
+		_putchar('\n');
+	}
+}
