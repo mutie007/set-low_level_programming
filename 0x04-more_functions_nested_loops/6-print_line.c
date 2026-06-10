da07950422c5ce6386ac432c0a50a8d2b691b731
mutie007 <your@email.com>
2026-06-10 13:59:43 +0300

    Add 6-print_line.c - draws a straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: number of times to print '_'
+ */
+void print_line(int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+		_putchar('_');
+		i++;
+	}
+	_putchar('\n');
+}
