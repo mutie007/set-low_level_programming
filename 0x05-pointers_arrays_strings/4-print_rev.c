8b551260f89c833ab36b3aac54be54cb7faa68c2
mutie007 <your@email.com>
2026-06-10 15:05:07 +0300

    Add 4-print_rev.c - prints a string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse followed by a new line
+ * @s: pointer to the string to print
+ */
+void print_rev(char *s)
+{
+	int len;
+
+	len = 0;
+	while (s[len] != '\0')
+		len++;
+
+	while (len > 0)
+	{
+		len--;
+		_putchar(s[len]);
+	}
+	_putchar('\n');
+}
