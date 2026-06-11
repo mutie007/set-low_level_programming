03d4a20824f13cb5c447c761052ae056d4d5dc3a
mutie007 <your@email.com>
2026-06-11 13:58:30 +0300

    Add 2-strncpy.c - copies a string using at most n bytes

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string using at most n bytes
+ * @dest: pointer to destination buffer
+ * @src: pointer to source string
+ * @n: maximum number of bytes to copy
+ * Return: pointer to dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
