e2ba768391237832188cd361ae5c4e822b908aa7
mutie007 <your@email.com>
2026-08-04 17:03:11 +0300

    Fix find_listint_loop - two variables only

--- more_singly_linked_lists/103-find_loop.c
+++ more_singly_linked_lists/103-find_loop.c
@@ -4,21 +4,21 @@
  * find_listint_loop - finds the loop in a linked list
  * @head: pointer to the head of the list
  *
- * Return: the address of the node where the loop starts,
- * or NULL if there is no loop
+ * Return: address of the node where the loop starts, or NULL
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *slow, *fast;
+	listint_t *slow = head;
+	listint_t *fast = head;
 
-	if (head == NULL || head->next == NULL)
+	if (head == NULL)
 		return (NULL);
 
-	slow = head->next;
-	fast = head->next->next;
-
-	while (fast && fast->next)
+	while (fast != NULL && fast->next != NULL)
 	{
+		slow = slow->next;
+		fast = fast->next->next;
+
 		if (slow == fast)
 		{
 			slow = head;
@@ -29,9 +29,6 @@ listint_t *find_listint_loop(listint_t *head)
 			}
 			return (slow);
 		}
-		slow = slow->next;
-		fast = fast->next->next;
 	}
-
 	return (NULL);
 }
