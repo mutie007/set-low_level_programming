aae587f8cf18cdbaa0d375b1d9c50c0011f0ef45
mutie007 <your@email.com>
2026-08-04 20:42:29 +0300

    Fix Betty style for sum_dlistint

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * sum_dlistint - returns the sum of all the data (n) of a dlistint_t linked list
+ * sum_dlistint - returns the sum of all the data (n) of a dlistint_t list
  * @head: pointer to the head of the list
  *
  * Return: sum of all the data (n), or 0 if the list is empty
@@ -15,5 +15,6 @@ int sum_dlistint(dlistint_t *head)
 		sum += head->n;
 		head = head->next;
 	}
+
 	return (sum);
 }
