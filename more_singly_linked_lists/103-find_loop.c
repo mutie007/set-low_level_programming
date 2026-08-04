9c431f076a844520d1370d1aba266d2a17debeb6
mutie007 <your@email.com>
2026-08-04 17:06:20 +0300

    find_listint_loop: exactly two variables, no malloc/free

--- more_singly_linked_lists/103-find_loop.c
+++ more_singly_linked_lists/103-find_loop.c
@@ -4,15 +4,19 @@
  * find_listint_loop - finds the loop in a linked list
  * @head: pointer to the head of the list
  *
- * Return: address of the node where the loop starts, or NULL
+ * Return: the address of the node where the loop starts,
+ * or NULL if there is no loop
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *slow = head;
-	listint_t *fast = head;
+	listint_t *slow;
+	listint_t *fast;
 
 	if (head == NULL)
 		return (NULL);
+
+	slow = head;
+	fast = head;
 
 	while (fast != NULL && fast->next != NULL)
 	{
