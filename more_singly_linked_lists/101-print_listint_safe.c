c2952ae1a1145030a310b2518ba6e3156717c934
mutie007 <your@email.com>
2026-08-04 16:32:33 +0300

    Add print_listint_safe function

--- more_singly_linked_lists/101-print_listint_safe.c
+++ more_singly_linked_lists/101-print_listint_safe.c
@@ -1,71 +1,51 @@
+#include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "lists.h"
 
 /**
- * is_looped - checks if a node was already visited
- * @visited: array of visited node pointers
- * @count: number of visited nodes
- * @node: node to check
+ * print_listint_safe - prints a listint_t linked list (safe version)
+ * @head: pointer to the head of the list
  *
- * Return: 1 if looped, 0 otherwise
- */
-static int is_looped(const listint_t **visited, size_t count,
-		const listint_t *node)
-{
-	size_t i;
-
-	i = 0;
-	while (i < count)
-	{
-		if (visited[i] == node)
-			return (1);
-		i++;
-	}
-	return (0);
-}
-
-/**
- * print_listint_safe - prints a listint_t linked list safely
- * @head: pointer to head of list
- *
- * Return: number of nodes in the list
+ * Return: the number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t **visited;
 	const listint_t *current;
-	size_t count;
-	size_t size;
+	const listint_t **addrs;
+	size_t i, count = 0;
+	size_t size = 10;
 
-	if (head == NULL)
-		return (0);
-	size = 1024;
-	visited = malloc(sizeof(listint_t *) * size);
-	if (visited == NULL)
+	addrs = malloc(sizeof(listint_t *) * size);
+	if (addrs == NULL)
 		exit(98);
-	count = 0;
+
 	current = head;
 	while (current != NULL)
 	{
-		if (is_looped(visited, count, current))
+		for (i = 0; i < count; i++)
 		{
-			printf("-> [%p] %d\n", (void *)current, current->n);
-			free(visited);
-			exit(98);
+			if (current == addrs[i])
+			{
+				printf("-> [%p] %d\n", (void *)current, current->n);
+				free(addrs);
+				return (count);
+			}
 		}
-		if (count >= size)
+
+		if (count == size)
 		{
 			size *= 2;
-			visited = realloc(visited, sizeof(listint_t *) * size);
-			if (visited == NULL)
+			addrs = realloc(addrs, sizeof(listint_t *) * size);
+			if (addrs == NULL)
 				exit(98);
 		}
-		visited[count] = current;
+
+		addrs[count] = current;
 		printf("[%p] %d\n", (void *)current, current->n);
 		count++;
 		current = current->next;
 	}
-	free(visited);
+
+	free(addrs);
 	return (count);
 }
