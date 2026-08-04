3a4ab3d4fe00e2a5fa87ab29610c160cdb6e42e1
mutie007 <your@email.com>
2026-08-04 16:09:49 +0300

    Add 101-print_listint_safe.c - prints linked list safely handling loops

--- more_singly_linked_lists/101-print_listint_safe.c
+++ more_singly_linked_lists/101-print_listint_safe.c
@@ -1,49 +1,56 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
 /**
  * print_listint_safe - prints a listint_t linked list safely
- * @head: pointer to the head of the list
+ * @head: pointer to head of list
  *
- * Return: the number of nodes in the list
+ * Return: number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
+	size_t count;
+	size_t i;
 	const listint_t *slow;
 	const listint_t *fast;
-	size_t count;
 
+	count = 0;
 	slow = head;
 	fast = head;
-	count = 0;
-
 	while (fast != NULL && fast->next != NULL)
 	{
-		printf("[%p] %d\n", (void *)slow, slow->n);
-		count++;
 		slow = slow->next;
 		fast = fast->next->next;
+		count++;
 		if (slow == fast)
 		{
-			printf("[%p] %d\n", (void *)slow, slow->n);
-			count++;
-			slow = slow->next;
+			slow = head;
+			i = 0;
 			while (slow != fast)
 			{
-				printf("[%p] %d\n", (void *)slow, slow->n);
-				count++;
 				slow = slow->next;
+				fast = fast->next;
+				i++;
+			}
+			count = i;
+			slow = head;
+			while (i > 0)
+			{
+				printf("[%p] %d\n", (void *)slow, slow->n);
+				slow = slow->next;
+				i--;
 			}
 			printf("-> [%p] %d\n", (void *)slow, slow->n);
-			return (count);
+			exit(98);
 		}
 	}
-	while (slow != NULL)
+	count = 0;
+	while (head != NULL)
 	{
-		printf("[%p] %d\n", (void *)slow, slow->n);
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
 		count++;
-		slow = slow->next;
 	}
 	return (count);
 }
--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -1,12 +1,11 @@
-#include <stdlib.h>
 #include "lists.h"
 
 /**
  * add_nodeint - adds a new node at the beginning of a listint_t list
- * @head: pointer to the pointer of the head of the list
- * @n: integer to store in the new node
+ * @head: pointer to pointer to head of list
+ * @n: integer value for new node
  *
- * Return: address of the new element, or NULL if it failed
+ * Return: address of new element, or NULL if failed
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
@@ -15,10 +14,8 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
-
 	new->n = n;
 	new->next = *head;
 	*head = new;
-
 	return (new);
 }
