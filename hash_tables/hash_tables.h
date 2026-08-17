7a6cde187ad62b321d8cd7d2437a027110d96a70
mutie007 <your@email.com>
2026-08-17 18:00:39 +0300

    Fix hash_tables.h corruption

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -67,36 +67,17 @@ typedef struct shash_table_s
 
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
-unsigned long int
-cat > 0-hash_table_create.c << 'EOF'
-#include "hash_tables.h"
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
+void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
 
-/**
- * hash_table_create - creates a hash table
- * @size: size of the array
- *
- * Return: pointer to the newly created hash table, or NULL on failure
- */
-hash_table_t *hash_table_create(unsigned long int size)
-{
-	hash_table_t *ht;
-	unsigned long int i;
+shash_table_t *shash_table_create(unsigned long int size);
+int shash_table_set(shash_table_t *ht, const char *key, const char *value);
+char *shash_table_get(const shash_table_t *ht, const char *key);
+void shash_table_print(const shash_table_t *ht);
+void shash_table_print_rev(const shash_table_t *ht);
+void shash_table_delete(shash_table_t *ht);
 
-	ht = malloc(sizeof(hash_table_t));
-	if (ht == NULL)
-		return (NULL);
-
-	ht->size = size;
-
-	ht->array = malloc(sizeof(hash_node_t *) * size);
-	if (ht->array == NULL)
-	{
-		free(ht);
-		return (NULL);
-	}
-
-	for (i = 0; i < size; i++)
-		ht->array[i] = NULL;
-
-	return (ht);
-}
+#endif
